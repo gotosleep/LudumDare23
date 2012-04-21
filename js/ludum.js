77a14c32dbef4499f6e4a311fbe4174a76670469
Jesse Andersen <gotosleep@gmail.com>
2012-04-21 16:36:28 -0700

    Adding laser eye

--- js/ludum.js
+++ js/ludum.js
@@ -39,6 +39,10 @@ $(document).ready(function () {
         building1C:[5, 0]
     });
 
+    Crafty.sprite(32, "images/laser.png", {
+        laserBeam:[0, 0]
+    });
+
     Crafty.c('Attached', {
         init:function () {
             this.bind('WorldMoved', function (movement) {
@@ -65,7 +69,7 @@ $(document).ready(function () {
             while (this.groundProgress < x) {
                 var type = Crafty.math.randomInt(1, 2) % 2 == 0 ? "grass" : "dirt";
                 type += Crafty.math.randomInt(1, 2);
-                Crafty.e("2D, DOM, solid, Attached, " + type)
+                Crafty.e("2D, DOM, solid, ground, Attached, " + type)
                     .attr({ 'x':this.groundProgress, y:config.height - 16, z:1 });
                 this.groundProgress += 16;
             }
@@ -91,7 +95,7 @@ $(document).ready(function () {
                                 type = y === 0 ? "building1T" : "building1C";
                             }
 
-                            Crafty.e("2D, DOM, Attached, " + type)
+                            Crafty.e("2D, DOM, explodable, Attached, " + type)
                                 .attr({ 'x':config.width + (x * this.building1Height), y:placement + (this.building1Height * y), 'z':z});
                         }
                     }
@@ -107,6 +111,25 @@ $(document).ready(function () {
             this.requires("SpriteAnimation, Collision")
                 .animate("walk", 0, 0, 1).stop().animate("walk", 20, -1);
 
+
+            //setup animations
+            this.requires("SpriteAnimation, Collision")
+                .animate("walk_left", 2, 0, 3)
+                .animate("walk_right", 0, 0, 1)
+                .stop().animate("walk_right", 20, -1)
+
+                //change direction when a direction change event is received
+                .bind("NewDirection",
+                function (direction) {
+                    if (direction.x < 0) {
+                        if (!this.isPlaying("walk_left"))
+                            this.stop().animate("walk_left", 20, -1);
+                    }
+                    if (direction.x > 0) {
+                        if (!this.isPlaying("walk_right"))
+                            this.stop().animate("walk_right", 20, -1);
+                    }
+                });
 
             // A rudimentary way to prevent the user from passing solid areas
             this.bind('Moved',
@@ -127,6 +150,63 @@ $(document).ready(function () {
         }
     });
 
+    Crafty.c('Laser', {
+        _speedX:8,
+        _speedY:7,
+        _life:5,
+        init:function () {
+            this.requires("2D, DOM, SpriteAnimation, laserBeam, Collision");
+            this.crop(0, 0, 32, 6);
+        },
+        start:function (direction) {
+            this.bind('EnterFrame', function () {
+                var change = direction === "right" ? this._speedX : -1 * this._speedX;
+                this.attr({x:this.x + change, y:this.y + this._speedY});
+                var laser = this;
+                $.each(this.hit("explodable"), function (index, value) {
+                    value.obj.destroy();
+                    laser._life -= 1;
+                    if (laser._life <= 0) {
+                        return false;
+                    }
+                });
+
+                if (this.hit("ground") || this._life <= 0 || this.x > config.width || (this.x + this.w) < 0) {
+                    this.destroy();
+                }
+            });
+        }
+    });
+
+
+    Crafty.c('LaserShooter', {
+        _key:Crafty.keys.SPACE,
+        _direction:"right",
+
+        init:function () {
+            var shooter = this;
+            //Create the bomb
+            this.bind('KeyDown', function (e) {
+                if (e.key !== this._key) {
+                    return;
+                }
+                var laser = Crafty.e('Laser');
+                var x = ((shooter.w - laser.w) / 2) + shooter.x;
+                laser.attr({z:shooter.z, 'x':x, y:shooter.y + 21});
+                laser.start(this._direction);
+            });
+
+            //change direction when a direction change event is received
+            this.bind("NewDirection",
+                function (direction) {
+                    if (direction.x < 0) {
+                        this._direction = "left";
+                    } else if (direction.x > 0) {
+                        this._direction = "right";
+                    }
+                });
+        }
+    });
 
     Crafty.c("RightControls", {
         init:function () {
@@ -144,16 +224,16 @@ $(document).ready(function () {
 
     //the loading screen that will display while our assets load
     Crafty.scene("loading", function () {
-        //load takes an array of assets and a callback when complete
-        Crafty.load(["images/robot.png", "images/ground.png", "images/building.png"], function () {
-            Crafty.scene("main"); //when everything is loaded, run the main scene
-        });
-
         //black background with some loading text
         Crafty.background("#000");
         Crafty.e("2D, DOM, Text").attr({ w:100, h:20, x:150, y:120 })
             .text("Loading")
             .css({ "text-align":"center", "color":"white" });
+
+        //load takes an array of assets and a callback when complete
+        Crafty.load(["images/robot.png", "images/ground.png", "images/building.png", "images/laser.png"], function () {
+            Crafty.scene("main"); //when everything is loaded, run the main scene
+        });
     });
 
     Crafty.scene("main", function () {
@@ -162,7 +242,7 @@ $(document).ready(function () {
         Generator.ground(config.width);
         Crafty.background("#FFF");
 
-        var player = Crafty.e("2D, DOM, Ape, robot, RightControls, SpriteAnimation, Collision, BombDropper, Grid")
+        var player = Crafty.e("2D, DOM, Ape, robot, RightControls, SpriteAnimation, Collision, LaserShooter, Grid")
             .attr({ x:150, y:150, z:2 })
             .rightControls(4)
             .Ape();
